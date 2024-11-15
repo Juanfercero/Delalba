0b8735708786e71f7cb9ded4f376a9c27fc66557
Juanfercero <162922620+Juanfercero@users.noreply.github.com>
2024-11-15 11:54:59 -0300

    Maestro Creado

--- Delalba/Components/Pages/Productos/ProductosPage.razor.cs
+++ Delalba/Components/Pages/Productos/ProductosPage.razor.cs
@@ -6,7 +6,7 @@ namespace Delalba.Components.Pages.Productos
     {
         private string MensajeError = "";
         private bool EstamosModificando = false;
-        private ProductoEntity? PeliculasModificando;
+        private ProductoEntity? ProductoModificando;
 
         private int Precio = 0;
         private string Nombre = "";
@@ -35,6 +35,38 @@ namespace Delalba.Components.Pages.Productos
                 MensajeError = "";
             }
 
+        }
+        private void Modificar(ProductoEntity productoModificar)
+        {
+            EstamosModificando = true;
+            ProductoModificando = productoModificar;
+
+            Nombre = productoModificar.Nombre;
+            Precio = productoModificar.Precio;
+        }
+
+        private void GuardarCambios()
+        {
+            if (Nombre == "" || Precio == 0)
+            {
+                MensajeError = "Faltaron datos por ingresar!";
+            }
+            else
+            {
+                ProductoModificando.Nombre = Nombre;
+                ProductoModificando.Precio = Precio;
+
+                Nombre = "";
+                Precio = 0;
+
+                EstamosModificando = false;
+                ProductoModificando = null;
+            }
+        }
+
+        private void Eliminar(ProductoEntity productoEliminar)
+        {
+            ProductosList.Remove(productoEliminar);
         }
     }
 }
